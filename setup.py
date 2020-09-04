8b878a7c4b53096520997660770139759e8e3839
Geoffrey Spear <geoffspear@gmail.com>
2020-09-04 08:47:57 -0400

    style

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='marctools',
-    version='0.1',
-    packages=[''],
-    url='',
-    license='MIT',
-    author='Geoffrey Spear',
-    author_email='speargh@pitt.edu',
-    description='Command line tools for MARC21 records'
+    name="marctools",
+    version="0.1",
+    packages=[""],
+    url="",
+    license="MIT",
+    author="Geoffrey Spear",
+    author_email="speargh@pitt.edu",
+    description="Command line tools for MARC21 records",
+    install_requires=["pymarc"],
 )
