00793a0906937f546d51805532eff0e76bdad299
Geoffrey Spear <geoffspear@gmail.com>
2020-09-04 08:41:27 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='marctools',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Geoffrey Spear',
+    author_email='speargh@pitt.edu',
+    description='Command line tools for MARC21 records'
+)
